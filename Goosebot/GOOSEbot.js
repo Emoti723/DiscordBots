09f9989c768122308e970d18c51d32fecf8ba75d
Emoti723 <44149950+Emoti723@users.noreply.github.com>
2020-05-07 11:35:11 -0600

    V3.5
    Added log4js in order to log info
    Added Momentjs in order to log time
    
    Updated the Readme

--- Goosebot/GOOSEbot.js
+++ Goosebot/GOOSEbot.js
@@ -1,24 +1,39 @@
 //GOOSEbot @Emoti723#6885
-//V3.0
+//V3.5
+//See README.MD for details
 
+var moment = require('moment')
+const myLoggers = require('log4js');
 const Discord = require('discord.js');
 const client = new Discord.Client()
 
+
+//Log File code
+myLoggers.configure({
+    appenders: { mylogger: { type:"file", filename: "log.txt" } },
+    categories: { default: { appenders:["mylogger"], level:"ALL" } }
+});
+const logger = myLoggers.getLogger("default");
+
+//Timestamp Code
+var created = moment().format('YYYY-MM-DD hh:mm:ss')
+
 var sum = 0;
+var prefix = 'G:sum';
 
 // Initialization 
 client.on("ready", () =>{
     console.log(`Logged in as GOOSEBot#5787!`);
 	console.log(sum);
-    client.user.setPresence({
-        status: "online",  //You can show online, idle....
-        game: {
-            name: "G:sum",  //The message shown
-            type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
-        }
-    });
+	console.log(created);
+	logger.info(`Logged in as GOOSEBot#5787!`);
+	logger.info(sum);
+	logger.info(created);
+	client.user.setActivity(`prefix: ${prefix}`, { type: 'STREAMING' });
+	});
  });
  
+
 // Test for the word HONK and reply with GOOSE
 client.on('message', msg => {
 	var string = msg.content.toUpperCase();
@@ -35,17 +50,24 @@ client.on('message', msg => {
     msg.reply(sum);
     console.log('sum has been requested');
     console.log(sum);
+	console.log(created);
+	logger.info('sum has been requested');
+	logger.info(sum);
+	logger.info(created);
   }
 });
 
 // After 5 minutes, log the sum to console
 var minutes = 5, the_interval = minutes * 60 * 1000;
 setInterval(function() {
-   console.log('Its been 5 minutes');
-   console.log(sum); 
+	console.log('Its been 5 minutes');
+	console.log(sum); 
+	console.log(created);
+	logger.info('its been 5 minutes');
+	logger.info(sum);
+	logger.info(created);
 }, the_interval);
 
 //token
 const config = require("./TOKEN.json");
 client.login(config.token); // Uses value of key 'token' in the TOKEN file. 
-
--- Honkbot/HONKbot.js
+++ Honkbot/HONKbot.js
@@ -1,24 +1,39 @@
 //HONKbot @Emoti723#6885
-//V3.0
+//V3.5
+//See README.MD for details
 
+const myLoggers = require('log4js');
+var moment = require('moment')
 const Discord = require('discord.js');
 const client = new Discord.Client()
 
+
+//Log File code
+myLoggers.configure({
+    appenders: { mylogger: { type:"file", filename: "log.txt" } },
+    categories: { default: { appenders:["mylogger"], level:"ALL" } }
+});
+const logger = myLoggers.getLogger("default");
+
+//Timestamp Code
+var created = moment().format('YYYY-MM-DD hh:mm:ss')
+
 var sum = 0;
+var prefix = 'H:sum';
 
 // Initialization 
 client.on("ready", () =>{
     console.log(`Logged in as HONKBot#9853!`);
 	console.log(sum);
-    client.user.setPresence({
-        status: "online",  //You can show online, idle....
-        game: {
-            name: "H:sum",  //The message shown
-            type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
-        }
-    });
+	console.log(created);
+	logger.info(`Logged in as HONKBot#9853!`);
+	logger.info(sum);
+	logger.info(created);
+	client.user.setActivity(`prefix: ${prefix}`, { type: 'STREAMING' });
+	});
  });
  
+
 // Test for the word GOOSE and reply with HONK
 client.on('message', msg => {
 	var string = msg.content.toUpperCase();
@@ -35,17 +50,20 @@ client.on('message', msg => {
     msg.reply(sum);
     console.log('sum has been requested');
     console.log(sum);
+	logger.info('sum has been requested');
+	logger.info(sum);
   }
 });
 
 // After 5 minutes, log the sum to console
 var minutes = 5, the_interval = minutes * 60 * 1000;
 setInterval(function() {
-   console.log('Its been 5 minutes');
-   console.log(sum); 
+	console.log('Its been 5 minutes');
+	console.log(sum); 
+	logger.info('its been 5 minutes');
+	logger.info(sum);
 }, the_interval);
 
 //token
 const config = require("./TOKEN.json");
 client.login(config.token); // Uses value of key 'token' in the TOKEN file. 
-
