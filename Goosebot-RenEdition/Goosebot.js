fe5e34d1e217d245e73ddbb30fa88e5f0bd184e8
Emoti723 <44149950+Emoti723@users.noreply.github.com>
2020-10-15 11:17:04 -0600

    Update Goosebot.js

--- Goosebot-RenEdition/Goosebot.js
+++ Goosebot-RenEdition/Goosebot.js
@@ -10,12 +10,9 @@ const client = new Discord.Client()
 //Timestamp variable
 var created = moment().format('YYYY-MM-DD hh:mm:ss');
 
-//Timestamp variable
-var created = moment().format('YYYY-MM-DD hh:mm:ss');
-
 //Log File code
 myLoggers.configure({
-    appenders: { mylogger: { type:"file", filename:"${created}-log.txt" } },
+    appenders: { mylogger: { type:"file", filename:`${created} log.txt` } },
     categories: { default: { appenders:["mylogger"], level:"ALL" } }
 });
 
